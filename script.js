94df73269d3ba0fddddec4fb87d154fe0fab310a
Abdyukov Zufar <swat55551@gmail.com>
2023-09-13 15:20:44 +0300

    Start front Show board

---
+++ script.js
@@ -0,0 +1,33 @@
+class Piece
+{
+    constructor(row,column)
+    {
+        this.row = row;
+        this.column = column;
+    }
+
+    compare(piece)
+    {
+        return piece.row === this.row && piece.column === this.column;
+    }
+
+}
+
+const modal = document.getElementById("easyModal");
+let game = document.getElementById("game");
+let currentPlayer = 1;
+const posNewPosition = [];
+const capturedPosition = [];
+const board = [
+    [0, -1, 0, -1, 0, -1, 0, -1, 0, -1],
+    [-1, 0, -1, 0, -1, 0, -1, 0, -1, 0],
+    [0, -1, 0, -1, 0, -1, 0, -1, 0, -1],
+    [-1, 0, -1, 0, -1, 0, -1, 0, -1, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+    [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+];
+
